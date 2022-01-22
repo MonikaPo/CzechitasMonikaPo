c90a10a615f914469bfefac7440bdec553fa9104
MonikaPo <monikapouzarovamp@gmail.com>
2022-01-22 11:03:01 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 import './ukol1.js';
+//Tento prikaz neco vypise do konzole
+
+let jmeno = 'Monulenka';
+let povolani = 'tester';
+
+let currentUser
+
+// boolean - true / false
+let jeZima = true;
+
+//1) zacinat pismenem
+//2) obsahuje pismenka a cislice
+//3) 
+
+console.log('Monička')
+console.log(3336374842)
+
+let a = 5;
+let b = 5;
+let soucet = a + b;
+console.log(soucet)
+
+
+let c = 3 > 2; //boolean - pravdivostni hodnota - pravda / nepravda
+console.log(c);
---
+++ ukol2.js
@@ -0,0 +1,6 @@
+let jmeno = 'Petra';
+let vek = '34';
+let vaha = '65';
+let vyska = "1.67";
+let BMI = vaha/(1.67*1.67);
+console.log('Jmenuji se' + jmeno + 'a je mi' + vek + 'a vážím' + váha + 'a moje BMI je' + BMI);
